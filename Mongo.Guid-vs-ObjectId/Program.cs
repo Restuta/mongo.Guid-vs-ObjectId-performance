61724ef6b2d05b64358a67feed57ad233fe51b4f
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-06 15:30:07 -0800

    initial commit

---
+++ Mongo.Guid-vs-ObjectId/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo.Guid_vs_ObjectId
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
