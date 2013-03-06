3ed5e8e093e8371ba1c06aadf59e8be678b0f934
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-06 15:40:11 -0800

    added ColorfullConsole and few delegates

--- Mongo.Guid-vs-ObjectId/Program.cs
+++ Mongo.Guid-vs-ObjectId/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Restuta.ConsoleExtensions.Colorfull;
 
 namespace Mongo.Guid_vs_ObjectId
 {
+    internal delegate void Write(ColorfullString colorfullString);
+    internal delegate void WriteLine(ColorfullString colorfullString);
+        
     class Program
     {
+        static readonly Write W = ColorfullConsole.Write;
+        static readonly WriteLine WL = ColorfullConsole.WriteLine;
+
         static void Main(string[] args)
         {
+            WL("test".Red());
         }
     }
 }
