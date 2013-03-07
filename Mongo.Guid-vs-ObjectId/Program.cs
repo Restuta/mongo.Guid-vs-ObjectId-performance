8343fedf9960643d4f8bfd3475c972913904a0ea
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-07 14:46:30 -0800

    objectId test code

--- Mongo.Guid-vs-ObjectId/Program.cs
+++ Mongo.Guid-vs-ObjectId/Program.cs
@@ -15,10 +15,10 @@ namespace Mongo.Guid_vs_ObjectId
     internal delegate void Write(ColorfullString colorfullString);
     internal delegate void WriteLine(ColorfullString colorfullString);
 
-    public class TestDocument
+    public class TestDocumentObjectId
     {
-        [BsonId(IdGenerator = typeof(GuidGenerator))]
-        public Guid Id { get; set; }
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        public ObjectId Id { get; set; }
     }
 
     class Program
@@ -31,41 +31,41 @@ namespace Mongo.Guid_vs_ObjectId
         {
             Measure.WhenDone = (prefix, time) => WL(prefix.ToString().DarkCyan() + " time: " + (time.ToString() + "ms").DarkYellow());
 
-            const int NumberOfDocuments = 10000000;
+            const int NumberOfDocuments = 26402104;
             var db = GetDatabase();
 
-            Measure.Performance(NumberOfDocuments + " documents insertion with", () =>
-            {
-                foreach (var document in NewTestDocument().Take(NumberOfDocuments))
-                {
-                    db.GetCollection<TestDocument>().Insert(document);
-                }
-            });
-            WL((Measure.LastMeasurmentTime / Convert.ToDouble(NumberOfDocuments)).ToString() + "ms per document");
-
-            //Measure.Performance(NumberOfDocuments + " BATCH documents insertion with", () =>
+            //Measure.Performance(NumberOfDocuments + " documents insertion with", () =>
             //{
-            //    db.GetCollection<TestDocument>().InsertBatch(NewTestDocument().Take(NumberOfDocuments));
+            //    foreach (var document in NewTestDocument().Take(NumberOfDocuments))
+            //    {
+            //        db.GetCollection<TestDocumentObjectId>().Insert(document);   
+            //    }
             //});
             //WL((Measure.LastMeasurmentTime / Convert.ToDouble(NumberOfDocuments)).ToString() + "ms per document");
 
+            Measure.Performance(NumberOfDocuments + " BATCH documents insertion with", () =>
+            {
+                db.GetCollection<TestDocumentObjectId>().InsertBatch(NewTestDocument().Take(NumberOfDocuments));
+            });
+            WL((Measure.LastMeasurmentTime / Convert.ToDouble(NumberOfDocuments)).ToString() + "ms per document");
+
             Measure.Performance("Skip 10 000 000 docs and take one", () =>
             {
-                var doc = db.GetCollection<TestDocument>().AsQueryable().Skip(10000000).Take(1).ToList().SingleOrDefault();
+                var doc = db.GetCollection<TestDocumentObjectId>().AsQueryable().Skip(10000000).Take(1).ToList().SingleOrDefault();
                 WL(doc.Id.ToString());
             });
 
             Measure.Performance("Reading one document by id", () =>
             {
-                var doc = db.GetCollection<TestDocument>().AsQueryable().SingleOrDefault(x => x.Id == new Guid("2dbe6ee1-42b7-4209-8c84-cb2fbe6a799c"));
+                var doc = db.GetCollection<TestDocumentObjectId>().AsQueryable().SingleOrDefault(x => x.Id == new ObjectId("5137e369932bdc677423e80d"));
                 WL(doc.Id.ToString());
             });
 
-            //Measure.Performance("Count doc's grater than random Id", () =>
-            //{
-            //    var doc = db.GetCollection<TestDocument>().AsQueryable().Count(x => x.Id >= new Guid("2dbe6ee1-42b7-4209-8c84-cb2fbe6a799c"));
-            //    WL(doc.ToString());
-            //});
+            Measure.Performance("Reading one document by id", () =>
+            {
+                var result = db.GetCollection<TestDocumentObjectId>().AsQueryable().Select(x => x.Id).GroupBy(x => x).Count();
+                WL(result.ToString());
+            });
 
         }
 
@@ -81,11 +81,11 @@ namespace Mongo.Guid_vs_ObjectId
         }
 
 
-        public static IEnumerable<TestDocument> NewTestDocument()
+        public static IEnumerable<TestDocumentObjectId> NewTestDocument()
         {
             while (true)
             {
-                yield return new TestDocument();
+                yield return new TestDocumentObjectId();
             }
         }
     }
