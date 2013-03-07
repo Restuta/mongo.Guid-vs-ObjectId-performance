4ffb46113b132821affb64f4b2e205ecb2190419
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-06 16:06:48 -0800

    added mongo db initialization code

---
+++ Mongo.Guid-vs-ObjectId/Measure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mongo.Guid_vs_ObjectId
+{
+    public static class Measure
+    {
+        private static readonly System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        
+        /// <summary>
+        /// Will be executed at the end of each performance measurement, by default writes elapsed ms to Console
+        /// </summary>
+        public static Action<string, long> WhenDone = (prefix, elapsedTime) => Console.WriteLine(elapsedTime + "ms");
+
+        public static void Performance(Action codeToMeasure, string prefix = "")
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            codeToMeasure.Invoke();
+
+            stopwatch.Stop();
+
+            WhenDone.Invoke(prefix, stopwatch.ElapsedMilliseconds);
+        }
+
+
+        public static void Performance(string prefix, Action codeToMeasure)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            codeToMeasure.Invoke();
+
+            stopwatch.Stop();
+
+            WhenDone.Invoke(prefix, stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
--- Mongo.Guid-vs-ObjectId/Program.cs
+++ Mongo.Guid-vs-ObjectId/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 using Restuta.ConsoleExtensions.Colorfull;
 
 namespace Mongo.Guid_vs_ObjectId
@@ -15,9 +16,34 @@ namespace Mongo.Guid_vs_ObjectId
         static readonly Write W = ColorfullConsole.Write;
         static readonly WriteLine WL = ColorfullConsole.WriteLine;
 
+
         static void Main(string[] args)
         {
-            WL("test".Red());
+            Measure.WhenDone = (prefix, time) => WL(prefix.ToString().DarkCyan() + " time: " + (time.ToString() + "ms").DarkYellow());
+
+
+            
+            var db = GetDatabase();
+
+            
+            
+            //actual sugar
+            Measure.Performance("1M document insertion with GUIDs", () =>
+            {
+                
+            });
+        }
+
+        private static MongoDatabase GetDatabase()
+        {
+            var testDbConnStrBuilder = new MongoUrlBuilder("mongodb://localhost/Guid-vs-ObjectId?safe=true");
+            var connectionString = testDbConnStrBuilder.ToMongoUrl();
+            var client = new MongoClient(connectionString);
+            var mongoDatabase = client.GetServer().GetDatabase(connectionString.DatabaseName);
+
+            mongoDatabase.SetProfilingLevel(ProfilingLevel.All);
+            return mongoDatabase;
+
         }
     }
 }
