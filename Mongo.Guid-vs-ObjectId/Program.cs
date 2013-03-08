88a9bb6884889d162e51d70816013a9f7bc826d6
Anton Vynogradenko <anton.vynogradenko@frontrange.com>
2013-03-08 13:03:03 -0800

    added Threading.Timer to report progress each N seconds

--- Mongo.Guid-vs-ObjectId/Program.cs
+++ Mongo.Guid-vs-ObjectId/Program.cs
@@ -9,6 +9,7 @@ using MongoDB.Bson.Serialization.IdGenerators;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using Restuta.ConsoleExtensions.Colorfull;
+using System.Threading;
 
 namespace Mongo.Guid_vs_ObjectId
 {
@@ -26,44 +27,61 @@ namespace Mongo.Guid_vs_ObjectId
         static readonly Write W = ColorfullConsole.Write;
         static readonly WriteLine WL = ColorfullConsole.WriteLine;
 
-
         static void Main(string[] args)
         {
             Measure.WhenDone = (prefix, time) => WL(prefix.ToString().DarkCyan() + " time: " + (time.ToString() + "ms").DarkYellow());
 
-            const int NumberOfDocuments = 26402104;
+            const int NumberOfDocuments = 100 * 1000000;
+            const int NotificationInterval = 60 * 1000;
             var db = GetDatabase();
+            var mongoCollection = db.GetCollection<TestDocumentObjectId>();
+
+            long previousDocsCount = mongoCollection.Count();
+
+            var timer = new Timer(
+                state =>
+                {
+                    var docsCount = mongoCollection.Count();
+                    var docsCountDelta = docsCount - previousDocsCount;
+                    previousDocsCount = docsCount;
+                    WL("docs inserted during past " + NotificationInterval / 1000 + " seconds:" + docsCountDelta.ToString().Green());
+                },
+                state: null,
+                period: NotificationInterval,
+                dueTime: 1);
 
             //Measure.Performance(NumberOfDocuments + " documents insertion with", () =>
             //{
             //    foreach (var document in NewTestDocument().Take(NumberOfDocuments))
             //    {
-            //        db.GetCollection<TestDocumentObjectId>().Insert(document);   
+            //        db.GetCollection<TestDocumentObjectId>().Insert(document);
             //    }
             //});
             //WL((Measure.LastMeasurmentTime / Convert.ToDouble(NumberOfDocuments)).ToString() + "ms per document");
 
             //Measure.Performance(NumberOfDocuments + " BATCH documents insertion with", () =>
             //{
-            //    db.GetCollection<TestDocumentObjectId>().InsertBatch(NewTestDocument().Take(NumberOfDocuments));
+            //    mongoCollection.InsertBatch(NewTestDocument().Take(NumberOfDocuments));
             //});
             //WL((Measure.LastMeasurmentTime / Convert.ToDouble(NumberOfDocuments)).ToString() + "ms per document");
 
+            timer.Change(0, Timeout.Infinite);
+
             Measure.Performance("Skip 10 000 000 docs and take one", () =>
             {
-                var doc = db.GetCollection<TestDocumentObjectId>().AsQueryable().Skip(10000000).Take(1).ToList().SingleOrDefault();
+                var doc = mongoCollection.AsQueryable().Skip(10000000).Take(1).ToList().SingleOrDefault();
                 WL(doc.Id.ToString());
             });
 
             Measure.Performance("Reading one document by id", () =>
             {
-                var doc = db.GetCollection<TestDocumentObjectId>().AsQueryable().SingleOrDefault(x => x.Id == new ObjectId("5137e369932bdc677423e80d"));
+                var doc = mongoCollection.AsQueryable().SingleOrDefault(x => x.Id == new ObjectId("5137e369932bdc677423e80d"));
                 if (doc != null) WL(doc.Id.ToString());
             });
 
             Measure.Performance("Count doc's grater than random Id", () =>
             {
-                var doc = db.GetCollection<TestDocumentObjectId>().AsQueryable().Count(x => x.Id >= new ObjectId("5137e369932bdc677423e80d"));
+                var doc = mongoCollection.AsQueryable().Count(x => x.Id >= new ObjectId("5137e369932bdc677423e80d"));
                 WL(doc.ToString());
             });
 
